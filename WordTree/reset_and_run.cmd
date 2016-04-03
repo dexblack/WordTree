4a96d1019f2439533c70d22ca3429df6a8e982cd
dexblack <dex@dexblack.net>
2016-04-04 01:10:32 +1000

    Tightened up the test/debug execution script.

--- WordTree/reset_and_run.cmd
+++ WordTree/reset_and_run.cmd
@@ -1,6 +1,7 @@
 @echo off
-set THIS=%~p0
-set MNG=%THIS%\env\Scripts\python.exe manage.py
+set THISDIR=%~dp0
+set MNG="%THISDIR%env\Scripts\python.exe" manage.py
 if exist db.sqlite3 del db.sqlite3
 %MNG% migrate
 %MNG% runserver
+pause
