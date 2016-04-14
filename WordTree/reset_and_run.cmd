7a5eb6b65bd4111b0fdc7ba4266e86c0a99acbd6
dexblack <dex@dexblack.net>
2016-04-14 23:23:15 +1000

    Added createsuperuser operation to the launch script.

--- WordTree/reset_and_run.cmd
+++ WordTree/reset_and_run.cmd
@@ -3,5 +3,6 @@ set THISDIR=%~dp0
 set MNG="%THISDIR%env\Scripts\python.exe" manage.py
 if exist db.sqlite3 del db.sqlite3
 %MNG% migrate
+%MNG% createsuperuser --username ru --email dex@dexblack.net
 %MNG% runserver
 pause
