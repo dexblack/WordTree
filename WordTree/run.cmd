c88d37d8c1f7da7bb121a8cb75eb6e022641e94b
dexblack <dex@dexblack.net>
2016-05-23 19:23:43 +1000

    Added 'run.cmd' needed for file system deployment scenario.

---
+++ WordTree/run.cmd
@@ -0,0 +1,7 @@
+@echo off
+setlocal
+set THISDIR=%~dp0
+rem echo %THISDIR%
+start "RUMenuEditor" %THISDIR%\env\Scripts\python.exe manage.py runserver 0.0.0.0:8000
+explorer http://127.0.0.1:8000
+endlocal
