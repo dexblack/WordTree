6635f7a892614512719d9068ce52512f198d8d5b
dexblack <dex@dexblack.net>
2016-06-16 12:51:08 +1000

    Cope with spaces in path

--- WordTree/run.cmd
+++ WordTree/run.cmd
@@ -2,6 +2,6 @@
 setlocal
 set THISDIR=%~dp0
 rem echo %THISDIR%
-start "RUMenuEditor" %THISDIR%\env\Scripts\python.exe manage.py runserver 0.0.0.0:8000
+start "RUMenuEditor" "%THISDIR%\env\Scripts\python.exe" manage.py runserver 0.0.0.0:8000
 explorer http://127.0.0.1:8000
 endlocal
